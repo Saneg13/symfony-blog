b418d4d683420d0814afe847ba0b316b308a7fed
Saneg <SANEG@example.com>
2016-02-22 15:53:53 +0200

    first commit. Init

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on February 17, 2016, 10:29 am.
---
+++ src/Blogger/BlogBundle/Form/EnquiryType.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanek
+ * Date: 17.02.16
+ * Time: 13:15
+ */
+
+// src/Blogger/BlogBundle/Form/EnquiryType.php
+
+namespace Blogger\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class EnquiryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+        $builder->add('email', EmailType::class);
+        $builder->add('subject');
+        $builder->add('body', TextareaType::class);
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'contact';
+    }
+}
+
+// Old version
+//class EnquiryType extends AbstractType
+//{
+//    public function buildForm(FormBuilderInterface $builder, array $options)
+//    {
+//        $builder->add('name');
+//        $builder->add('email', 'email');
+//        $builder->add('subject');
+//        $builder->add('body', 'textarea');
+//    }
+//
+//    public function getName()
+//    {
+//        return 'contact';
+//    }
+//}
